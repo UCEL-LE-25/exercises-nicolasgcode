84fb1bd847ad58339011ccff3efea02dfa0666d6
nicolasgcode <contactnicolasgarcia@gmail.com>
2025-04-05 01:25:54 +0000

    Add count vocals ex | Update auth system ex imports

--- count_vocals.c
+++ count_vocals.c
@@ -1,26 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+
+  int vowels;
+  int consonants;
+} Category;
+
+Category classifyLetters(char text[]);
+
 int main(void) {
 
   char text[100];
-  int vocals = 0;
 
   printf("Enter a text: ");
   fgets(text, sizeof(text), stdin);
 
+  Category category = classifyLetters(text);
+
+  printf("\nThe given text has:\n %d Vowels\n %d Consonants\n", category.vowels,
+         category.consonants);
+
+  return 0;
+}
+
+Category classifyLetters(char text[]) {
+
+  Category category = {0, 0};
+
   for (int i = 0; i < strlen(text); i++) {
 
-    if (text[i] == 'A' || text[i] == 'a' || text[i] == 'E' || text[i] == 'e' ||
-        text[i] == 'I' || text[i] == 'i' || text[i] == 'O' || text[i] == 'o' ||
-        text[i] == 'U' || text[i] == 'u') {
-      vocals++;
-    } else {
-      vocals = 0;
+    if (text[i] != ' ' && text[i] != '\n') {
+      if (text[i] == 'A' || text[i] == 'a' || text[i] == 'E' ||
+          text[i] == 'e' || text[i] == 'I' || text[i] == 'i' ||
+          text[i] == 'O' || text[i] == 'o' || text[i] == 'U' ||
+          text[i] == 'u') {
+        category.vowels++;
+
+      } else {
+
+        category.consonants++;
+      }
     }
   }
 
-  printf("%d", vocals);
-
-  return 0;
+  return category;
 }
