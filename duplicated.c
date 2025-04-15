586b50ac4c95d9b3e94a076644d5253883da0b62
nicolasgcode <contactnicolasgarcia@gmail.com>
2025-04-15 19:04:45 +0000

    Duplicated ex: Program now finishes on first match

--- duplicated.c
+++ duplicated.c
@@ -7,19 +7,15 @@ int main(void) {
   int duplicated = 0;
 
   for (int i = 0; i < arrayLength; i++) {
-
-    for (int j = i + 1; j < arrayLength; j++) {
-      printf("%d\n", nums[i]);
-      printf("%d\n", nums[j]);
+    for (int j = i + 1; j < arrayLength && !duplicated; j++) {
+      printf("Iteration %d: %d and %d\n", i, nums[i], nums[j]);
       if (nums[i] == nums[j]) {
         duplicated = 1;
-      } else {
-        duplicated = 0;
       }
       break;
     }
   }
-  duplicated ? printf("True") : printf("False");
+  duplicated ? printf("True\n") : printf("False\n");
 
   return 0;
 }
