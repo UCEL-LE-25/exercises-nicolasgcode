54f966813c478590f3768f216bab3ca709359500
nicolasgcode <contactnicolasgarcia@gmail.com>
2025-05-07 01:37:02 +0000

    Change: folder name

---
+++ cex-students/src/include/student.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define MAX_STUDENTS 100
+
+typedef struct {
+  char name[50];
+  int age;
+  float finalScore;
+} Student;
---
+++ cex-students/src/main.c
@@ -0,0 +1,12 @@
+#include "operations.h"
+#include "program.h"
+#include "student.h"
+
+int main(void) {
+
+  Student students[MAX_STUDENTS] = {0};
+
+  program(students);
+
+  return 0;
+}
