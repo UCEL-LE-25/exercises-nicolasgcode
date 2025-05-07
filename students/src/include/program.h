afc602c0b88c6702cda2302f4e560c27e83841b9
ng <122330397+nicolasgcode@users.noreply.github.com>
2025-05-06 22:40:41 -0300

    Delete old students directory

--- students/src/include/program.h
+++
@@ -1,4 +0,0 @@
-#pragma once 
-#include "student.h"
-
-void program(Student students[]);
--- students/src/include/student.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define MAX_STUDENTS 100
-
-typedef struct {
-  char name[50];
-  int age;
-  float finalScore;
-} Student;
